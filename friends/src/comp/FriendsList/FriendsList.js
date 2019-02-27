47ad7c75fddb16ddac57808487fad597b4103df6
x-zen <jdharris.x@gmail.com>
2019-02-27 14:06:47 -0600

    Set-up base files

---
+++ friends/src/comp/FriendsList/FriendsList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FriendsList extends Component {
+  render() {
+    return(
+      <div>
+        Friends List
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
