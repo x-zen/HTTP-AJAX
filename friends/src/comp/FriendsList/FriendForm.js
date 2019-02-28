7bff9063b7d56c563049e085ebe7c01d88952b56
x-zen <jdharris.x@gmail.com>
2019-02-28 16:48:32 -0600

    Bad wolf

--- friends/src/comp/FriendsList/FriendForm.js
+++ friends/src/comp/FriendsList/FriendForm.js
@@ -1,18 +1,65 @@
 import React from 'react';
 
-const FriendForm = props => {
-  return(
-    <div>
-      <h2>Add A Friend!</h2>
+class FriendForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newFriend: {
+        name: '',
+        age: '',
+        email: ''
+      }
+    };
+  }
 
-      <form>
-        <input type='text' name='name' placeholder='Name' />
-        <input type='text' name='age' placeholder='Age' />
-        <input type='text' name='email' placeholder='Email' />
-        <input type='submit' value='Add Friend!' />
-      </form>
-    </div>
-  );
-};
+  handleChange = e => {
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  postMessage = e => {
+    e.preventDefault();
+
+  };
+
+  render() {
+    return(
+      <div>
+        <h2>Add A Friend!</h2>
+
+        <form onSubmit={this.postMessage}>
+          <input
+            type='text'
+            name='name'
+            placeholder='Name'
+            onChange={this.handleChange}
+            value={this.state.newFriend.name}
+          />
+          <input
+            type='text'
+            name='age'
+            placeholder='Age'
+            onChange={this.handleChange}
+            value={this.state.newFriend.age}
+          />
+          <input
+            type='text'
+            name='email'
+            placeholder='Email'
+            onChange={this.handleChange}
+            value={this.state.newFriend.email}
+          />
+          <button className="submit-btn" type="submit">
+            POST
+          </button>
+        </form>
+      </div>
+    );
+  };
+}
 
 export default FriendForm;
